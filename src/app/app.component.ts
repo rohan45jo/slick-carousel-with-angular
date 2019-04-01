381f1df63a36e75591eb357aa037b37dd200713a
rohan45jo <rohansjoshi94@gmail.com>
2019-04-01 13:57:01 +0530

    fixes in carousel script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ngOnInit() {
+    var item_length = $('.your-class > div').length - 1;
+    $(document).ready(function() {
 
+      var slider = $('.your-class').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          infinite: false
+      });
+      slider.on('afterChange', function(event, slick, currentSlide, nextSlide) {
+          console.log(slider.slick('slickCurrentSlide'), item_length);
+          if (item_length === slider.slick('slickCurrentSlide')) {
+              slider.slick("slickSetOption", "autoplay", false, false);
+          }
+      });
+    });
+  }
 }
