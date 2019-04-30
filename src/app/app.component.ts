602daf9893900d0507b6afa7561e9b1b388ca844
rohan45jo <rohansjoshi94@gmail.com>
2019-04-30 11:38:52 +0530

    update slick vertically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,22 +13,24 @@ import 'slick-carousel/slick/slick-theme.css';
 export class AppComponent {
   ngOnInit() {
     var item_length = $('.your-class > div').length - 1;
-    $(document).ready(function() {
+    $(document).ready(function () {
 
       var slider = $('.your-class').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: true,
-          dots: true,
-          autoplay: true,
-          autoplaySpeed: 4000,
-          infinite: false
+        vertical: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        accessibility: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: false
       });
-      slider.on('afterChange', function(event, slick, currentSlide, nextSlide) {
-          console.log(slider.slick('slickCurrentSlide'), item_length);
-          if (item_length === slider.slick('slickCurrentSlide')) {
-              slider.slick("slickSetOption", "autoplay", false, false);
-          }
+      slider.on('afterChange', function (event, slick, currentSlide, nextSlide) {
+        console.log(slider.slick('slickCurrentSlide'), item_length);
+        if (item_length === slider.slick('slickCurrentSlide')) {
+          slider.slick("slickSetOption", "autoplay", false, false);
+        }
       });
     });
   }
